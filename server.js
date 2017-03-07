da54d78aa02d7857010f21e99fffecec960d34fa
Jason Morton <info@webbiz.ie>
2017-03-07 20:22:36 +0200

    Module 8

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+})
+
+app.post('/add', function (req, res) {
+  res.send('Data recieved ' + req.body.name);
 })
 
 app.listen(3005, function () {
