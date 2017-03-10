98afa10a0bb716313bd9cba50ca69b9a5fd139b2
Riccardo <riccardo.derrico@gmail.com>
2017-03-10 19:23:17 +0100

    M3R6

--- api/events/index.js
+++ api/events/index.js
@@ -3,40 +3,50 @@ const data = require('../data')
 const router = express.Router()
 const uuidV1 = require('uuid/v1')
 
-let db = data.events;
+let db = data.events
 
 const filterEvents = (eventId) => {
-  return data.events.filter((obj) => {
+  return db.filter((obj) => {
     return eventId == obj.id
   })
 }
+const filterNotEvents = (eventId) => {
+  return db.filter((obj) => {
+    return eventId != obj.id
+  })
+}
 router.get('/', (req, res) => {
-  res.send(data.events)
+  res.send(db)
 })
 
 router.get('/:id', (req, res) => {
   const event = filterEvents(req.params.id)
   res.send(event)
 })
-
-router.post('/', (req, res) => {
-  
-  let newEvent = req.body;
-  newEvent.id = uuidV1();
-
-  // const newEvent = {
-  //   ...postData, id: uuidV1()
-  // }
-
-  const newData = [...data.events, newEvent];
-  
-  db = newData;
-
-  res.json({ 
+router.put('/:id', (req, res) => {
+  let newEvent = req.body
+  const id = req.params.id
+  const oldEventsList = filterNotEvents(id)
+  const oldEvent = filterEvents(id)
+  console.log('oldEvent', oldEvent)
+  let newEvent1 = Object.assign({}, oldEvent[0], newEvent)
+  console.log('event', newEvent1)
+  const newData = [...oldEventsList, newEvent1]
+  db = newData
+  res.json({
     message: 'Event has been added',
     events: db
-  });
-
+  })
+})
+router.post('/', (req, res) => {
+  let newEvent = req.body
+  newEvent.id = uuidV1()
+  const newData = [...db, newEvent]
+  db = newData
+  res.json({
+    message: 'Event has been added',
+    events: db
+  })
 })
 
 module.exports = router
